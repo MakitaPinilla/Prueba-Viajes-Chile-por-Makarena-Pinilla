45b5d4bea1f21937c128d3f5054872273bc4fb30
makitapinilla <pinillaandrademakarena@gmail.com>
2024-03-05 10:41:46 -0300

    Se agrega componente JS tooltips

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//ACTIVAR TOOLTIPS
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//ACTIVAR POPOVER
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+$(function () {
+
+    $("#enviar").on("click", function (event) {
+        alert("El mensaje fue enviado correctamente");
+    });
+});
