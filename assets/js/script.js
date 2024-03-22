6a622fceae92885713f7d5c7d47d7bed53bb517c
makitapinilla <pinillaandrademakarena@gmail.com>
2024-03-22 19:02:24 -0300

    Se agregran componentes Bootstrap SASS

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,25 @@ $(function () {
         alert("El mensaje fue enviado correctamente");
     });
 });
+
+// JavaScript para abrir y cerrar el modal
+var modal = document.getElementById('miModal');
+var btn = document.getElementById('miBoton');
+var span = document.getElementsByClassName('cerrar')[0];
+
+// Cuando el usuario hace clic en el botón, abre el modal 
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+// Cuando el usuario hace clic en (x), cierra el modal
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+// Cuando el usuario hace clic fuera del modal, lo cierra
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
